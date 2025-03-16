95ecb7cba07322030ee10afa9297f80e1fd54942
vikas600 <153718486+vikas600@users.noreply.github.com>
2025-03-16 18:07:46 +0000

    conversion & operations

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -4,12 +4,12 @@
 // let score = undefined
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber =Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33 " => 33
@@ -19,7 +19,7 @@ console.log(valueInNumber);
 // let isLoggedIn =1
 let isLoggedIn =""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1=> true ; 0=> false
@@ -29,6 +29,46 @@ console.log(booleanIsLoggedIn);
 
 let someNumber =33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+// ************************ Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2); 
+// console.log(2-2); 
+// console.log(2*2); 
+// console.log(2**3); 
+// console.log(2/2); 
+// console.log(2%3); 
+
+let str1 = "hello"
+let str2 = " vikas"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2 + 2);
+// console.log(1+2 + "2");
+
+
+console.log(+true); 
+console.log(+""); 
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
