adb513045092f0e2f67613d99e99cfed462dfbba
vikas600 <153718486+vikas600@users.noreply.github.com>
2025-03-22 13:46:50 +0000

    loop Concepts

---
+++ Iteration/two.js
@@ -0,0 +1,21 @@
+// while
+let i=0;
+while(i <=10){
+    console.log(`Value of i ${i}`);
+    i+=2;
+    
+}
+
+let myArray = ["flash" , "batman" , "superman"]
+let index = 0;
+while(index < myArray.length){
+    console.log(myArray[index]);
+    index++;
+    
+}
+let score =11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+    
+} while (score <=10);
