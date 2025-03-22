8576d89dcbf4edbde8435469db1a854e2dedce25
vikas600 <153718486+vikas600@users.noreply.github.com>
2025-03-22 18:58:20 +0000

    for of,  for in , for each concepts

---
+++ Iteration/four.js
@@ -0,0 +1,18 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    swift:'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is of ${myObject[key]}`);
+    
+    
+}
+
+const arr= ["p" , "a" , "c" , "d"];
+
+for (const i in arr) {
+  console.log(i);
+  
+}
