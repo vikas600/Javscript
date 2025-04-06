a62c40bc683188b422991dc095dfc3a8dfd8f492
vikas600 <153718486+vikas600@users.noreply.github.com>
2025-04-06 18:03:33 +0000

    OOP concepts

--- Classes and OOP/OOP.js
+++ Classes and OOP/OOP.js
@@ -29,4 +29,4 @@ function User(username , loginCount, isLoggedIn){
 
 const userOne = new User("hitesh",12 ,true)
 const userTwo = new User("ChaiAurCode", 11, false)
-console.log(userOne.constructor);
+console.log(userOne.constructor );
