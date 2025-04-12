3f05d0c9813b244dfdf98b00eba0b0bf27a7c2f2
vikas600 <153718486+vikas600@users.noreply.github.com>
2025-04-12 10:14:13 +0000

    getter_setter concepts

---
+++ Classes and OOP/getter_setter.js
@@ -0,0 +1,22 @@
+class User{
+    constructor(email,password){
+        this.email = email;
+        this.password= password
+    }
+    get email(){
+        return this._email.toUpperCase();
+    }
+    set email(value){
+         this._email=value;
+    }
+    get password(){
+        return this._password.toUpperCase();
+    }
+    set password(value){
+        this._password =value;
+    }
+}
+
+const hitesh = new User("hitesh.ai", "123abx")
+console.log(hitesh.password);
+console.log(hitesh.email);
